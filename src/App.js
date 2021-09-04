a968a877186f3da9f60160754da9fa67d9fa5f94
lsavoie <twosavoie@gmail.com>
2021-09-03 19:15:56 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = { on: true } //changing state from false to true
+    render() {
+        let wallClass = "wall off" // let because wallClass changes
+        if (this.state.on) {
+            wallClass = "wall on"
+        }
+        return (
+            <div className={wallClass}>
+                <div className="switch-plate">
+                    <div className="screw" />
+                    <div className="switch">
+                        <div className="switch-handle" />
+                    </div>
+                    <div className="screw" />
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
