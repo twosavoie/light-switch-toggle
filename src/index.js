5887fa57239691d0959debfa43d237c082da09a6
lsavoie <twosavoie@gmail.com>
2021-09-03 19:23:40 -0500

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App.js';
-
-// must use this instead of stict mode since I'm not using the ... file
-ReactDOM.render(<App />, document.getElementById('root'))
-
